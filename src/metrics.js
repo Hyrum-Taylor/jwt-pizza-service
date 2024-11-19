cfdd823a2680bba01167118da917f953189b1cec
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2024-11-18 17:40:22 -0700

    debug(AWS) problems with config files on AWS

--- src/metrics.js
+++ src/metrics.js
@@ -61,7 +61,7 @@ class Metrics {
   }
 
   sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
-    console.log(config.metrics.source);
+    console.log(config.metrics);
     const metric = `${metricPrefix},source=${config.metrics.source},method=${httpMethod} ${metricName}=${metricValue}`;
 
     fetch(`${config.metrics.url}`, {
