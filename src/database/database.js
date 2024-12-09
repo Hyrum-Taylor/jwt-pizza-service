9861bc2edc607357d65c7c8f9d87f6f72bc606d5
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2024-12-09 12:04:07 -0700

    fix(SQL injection) UpdateUser allowed SQL injection, so I fixed it

--- src/database/database.js
+++ src/database/database.js
@@ -83,17 +83,12 @@ class DB {
   async updateUser(userId, email, password) {
     const connection = await this.getConnection();
     try {
-      const params = [];
       if (password) {
         const hashedPassword = await bcrypt.hash(password, 10);
-        params.push(`password='${hashedPassword}'`);
+        await this.query(connection, `UPDATE user SET password=? WHERE id=?`, [hashedPassword, userId]);
       }
       if (email) {
-        params.push(`email='${email}'`);
-      }
-      if (params.length > 0) {
-        const query = `UPDATE user SET ${params.join(', ')} WHERE id=${userId}`;
-        await this.query(connection, query);
+        await this.query(connection, `UPDATE user SET email=? WHERE id=?`, [email, userId]);
       }
       return this.getUser(email, password);
     } finally {
